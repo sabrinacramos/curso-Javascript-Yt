b77fecca30a24d8c21b11d5e0e071510cdc71469
sabrinacramos <scramos.dev@gmail.com>
2022-11-04 15:19:01 -0300

    atualizado

--- js/classes.js
+++ js/classes.js
@@ -8,3 +8,18 @@ Pode-se dizer que são "funções especiais" para a criação de objetos.
 Assim como uma fábrica da vida real precisa de maquinas para construir os objetos, as Classes no Javascript usam um método chamado constructor() para fabricar objetos. 
 
 */
+
+class Carro {
+    constructor(valor1, valor2, valor3) {
+        this.marca = valor1;
+        this.modelo = valor2;
+        this.ano = valor3;
+    }
+    buzina() {
+        return this.modelo + "buzinou: Biiiiiiiiiiiiii";
+    }
+}
+
+const uno = new Carro("fiat", "Uno", 2001);
+const gol = new Carro("volkswagen", "Gol", 2013);
+console.log(gol.buzina());
