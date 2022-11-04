bfa1375da495369b7f35408b8fa9f0f21bfd0ab8
sabrinacramos <scramos.dev@gmail.com>
2022-11-04 01:07:22 -0300

    atualizado

---
+++ js/classes.js
@@ -0,0 +1,10 @@
+/* CLASSES EM JAVASCRIPT
+
+As classes são um conceito antigo em programação e várias linguagens utilizam elas. Mas no Javascript isso é relativamente novo, por isso mesmo programadores experientes não sabem muito bem utilizar.
+
+Basicamente, as classes são como "fábricas" para criar objetos.
+Pode-se dizer que são "funções especiais" para a criação de objetos.
+
+Assim como uma fábrica da vida real precisa de maquinas para construir os objetos, as Classes no Javascript usam um método chamado constructor() para fabricar objetos. 
+
+*/
